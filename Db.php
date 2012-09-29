2b11b688751bc3d97e91cc7c8460192da7a6721e
Fabrizio Balliano <fabrizio.balliano@sevenlike.com>
2012-09-29 14:01:34 +0200

    doc added

--- Db.php
+++ Db.php
@@ -9,24 +9,51 @@
  */
 
 namespace ZFBridge;
+use \Zend\Db\Sql\Sql;
 
 class Db
 {
+	/**
+	 * @var \Zend\Db\Adapter\Adapter
+	 */
 	private $adapter = null;
 
+	/**
+	 * @param \Zend\Db\Adapter\Adapter $adapter
+	 */
 	public function __construct($adapter)
 	{
 		$this->adapter = $adapter;
 	}
 
+	/**
+	 * @return \Zend\Db\Adapter\Adapter
+	 */
 	public function getAdapter()
 	{
 		return $this->adapter;
 	}
 
+	/**
+	 * @return \Zend\Db\Sql\Select
+	 */
+	public function select()
+	{
+		$sql = new \Zend\Db\Sql\Sql($this->getAdapter());
+		return $sql->select();
+	}
+
+	/**
+	 * @param string $query
+	 * @param null|array $params
+	 * @return array
+	 */
 	public function fetchAll($query, $params = null)
 	{
-		$return = array();
+
+		if (is_object($query)) {
+			$query = $query->getSqlString();
+		}
 
 		$rs = $this->getAdapter()->query($query)->execute($params);
 		foreach ($rs as $row) {
@@ -36,6 +63,11 @@ class Db
 		return $return;
 	}
 
+	/**
+	 * @param string $query
+	 * @param null|array $params
+	 * @return array
+	 */
 	public function fetchPairs($query, $params = null)
 	{
 		$return = array();
@@ -49,6 +81,11 @@ class Db
 		return $return;
 	}
 
+	/**
+	 * @param string $query
+	 * @param null|array $params
+	 * @return string
+	 */
 	public function fetchOne($query, $params = null)
 	{
 		$rs = $this->getAdapter()->query($query)->execute($params);
@@ -60,6 +97,11 @@ class Db
 		return null;
 	}
 
+	/**
+	 * @param string $query
+	 * @param null|array $params
+	 * @return array
+	 */
 	public function fetchCol($query, $params = null)
 	{
 		$return = array();
@@ -73,18 +115,34 @@ class Db
 		return $return;
 	}
 
+	/**
+	 * @param string $table
+	 * @param array $fields
+	 * @return int
+	 */
 	public function insert($table, $fields)
 	{
 		$tg = new \Zend\Db\TableGateway\TableGateway($table, $this->getAdapter());
 		return $tg->insert($fields);
 	}
 
+	/**
+	 * @param string $table
+	 * @param array $fields
+	 * @param null|string $where
+	 * @return int
+	 */
 	public function update($table, $fields, $where = null)
 	{
 		$tg = new \Zend\Db\TableGateway\TableGateway($table, $this->getAdapter());
 		return $tg->update($fields, $where);
 	}
 
+	/**
+	 * @param string $table
+	 * @param null|string $where
+	 * @return int
+	 */
 	public function delete($table, $where = null)
 	{
 		$tg = new \Zend\Db\TableGateway\TableGateway($table, $this->getAdapter());
